5794701fd938bd1aa9ef4353a0bd39da7fd76afb
Timothy Do <tydo83@gmail.com>
2020-09-24 12:10:15 -0400

    I made two versions to solve why B is capitalized

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
 function fizzy(num) {
-
+//Simplest approach.
+  if (num % 3 === 0 && num % 5 === 0) {
+    return "FizzBuzz";
+  }
+  else if (num % 3 === 0) {
+    return "Fizz";
+  }
+  else if (num % 5 === 0) {
+    return "Buzz";
+  }
+  else {
+    return num;
+  }
 }
+
+// //Why B in Buzz is capitalized? (I don't know how to approach this)
+// function fizzy(num) {
+//   let Fizz = "Fizz"
+//   let Buzz = "Buzz"
+  
+//   if (num % 3 == 0 && num %5 == 0) {
+//     return Fizz+Buzz;
+//   }
+//   else if (num % 3 == 0) {
+//     return Fizz;
+//   }
+//   else if (num % 5 == 0) {
+//     return Buzz;
+//   }
+//   else {
+//     return num;
+//   }
+// }
 
 module.exports = {
   fizzy,
